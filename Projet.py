72123f3beb241741a593440a6873de2c3a5dec04
JustSaki <billietjustin@gmail.com>
2025-11-09 13:06:55 +0100

    Projet.py

--- Projet.py
+++ Projet.py
@@ -36,7 +36,7 @@ capacite = st.sidebar.select_slider("Number of guests:", capacity_sorted)
 locations = locations.query(f"capacity == {capacite}")
 
 price_sorted = sorted(Price)   
-Max= st.sidebar.select_slider("Price:", price_sorted)
+Max= st.sidebar.select_slider("Price (€):", price_sorted.2f)
 locations = locations.query(f"price <= {Max}")
 
 type_location = st.sidebar.multiselect("Type of accomodation:", Room_Type)
