1bc85a31689cb568e6630513a807b4debe365f8a
JustSaki <billietjustin@gmail.com>
2025-11-09 11:54:24 +0100

    Projet.py

--- Projet.py
+++ Projet.py
@@ -55,12 +55,12 @@ if ville and type_location :
         
         if nbr_L == 0 :
             st.info('No accomodations match your selection')
-        affichage = ["city", "price".round(2), "room_type", "bedrooms", "cleanliness_rating", "guest_satisfaction_overall",\
+        affichage = ["city", "price", "room_type", "bedrooms", "cleanliness_rating", "guest_satisfaction_overall",\
                      "dist_from_center", "dist_from_metro"]
         st.write(f"Available rental for {capacite} people with selected accomodations types: {type_location}.")
 #hauteur max comprise entre nbr de ligne * 40 et 400 (pixels)
         #h_max = min(nbr_L*40, 400)
-        st.dataframe(locations[affichage], height = "auto")
+        st.dataframe(locations[affichage],column_config={"price": st.column_config.NumberColumn("Price (€)", format="%.2f")} height = "auto")
         st.write(f"dist_from_center and dist_from_metro represent the distances from the accomodation to the city center\
                  and the nearest metro station, expressed in km.")
         
